47cdb5bbc34cfb0d770498b88ffd705f7f2de26b
sahanas7 <134706438+sahanas7@users.noreply.github.com>
2023-09-15 07:59:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+// const products =[
+//     {name: 'samsung s23 ultra', price:85000 },
+//     {name: 'laptop', price:35000 },
+//     {name: 'bike', price:100000},
+//     {name: 'keyboard', price:1000 },
+//     {name: 'tv', price:38000 },
+//     {name: 'tws', price:1500}
+// ]
+// const itemnames = products.map((product) =>{
+// return product.price
+// })
+// console.log(itemnames)
+
+
